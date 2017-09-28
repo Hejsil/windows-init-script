f965170d915188e06a87b08914bdabc49b3c63b7
Jimmi H C <jimmiholstchristensen@gmail.com>
2017-09-28 18:05:51 +0200

    Added lines for enabling windows developer mode

--- main.ps1
+++ main.ps1
@@ -119,5 +119,14 @@ foreach ($exe in $exes) {
     & "$PSScriptRoot\$tmp" | Out-Null
 }
 
+# Create AppModelUnlock if it doesn't exist, required for enabling Developer Mode
+$RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
+if (-not(Test-Path -Path $RegistryKeyPath)) {
+    New-Item -Path $RegistryKeyPath -ItemType Directory -Force
+}
+
+# Add registry value to enable Developer Mode
+New-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
+
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 pause
